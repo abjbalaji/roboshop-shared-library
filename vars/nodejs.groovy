89e40e6075b23664253a6a576c5a75cc5e440816
Balaji A <abj.balaji@gmail.com>
2022-05-18 23:09:43 +0530

    Updating

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -5,15 +5,35 @@ def lintChecks(){
        '''
         }
 
+
+def  sonarChecks(){
+    sh '''
+        sonar-scanner -Dsonar.host.url=http://172.31.9.128:9000 -Dsonar.sources=. -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.passwd=${SONAR_PSW}
+       '''
+}
+
 def call(){
     pipeline{
         agent any
+
+        environmentp {
+        SONAR = credentials('SONAR')
+        }
 
         stages{
             stage('Lint Checks') {
                 steps{
                     script{
                        lintChecks()
+                    }
+
+                }
+            }
+
+            stage('Sonar Checks') {
+                steps{
+                    script{
+                        sonarChecks()
                     }
 
                 }
