5a9c2236d8553ab299d1c0ddfbd1ef0f8433eb01
Balaji A <abj.balaji@gmail.com>
2022-05-18 21:56:36 +0530

    Updating

---
+++ vars/maven.groovy
@@ -0,0 +1,25 @@
+def lintChecks(){
+    sh '''
+      # ~/node_modules/jslint/bin/jslint.js server.js
+      mvn checkstyle:check
+       '''
+        }
+
+def call(){
+    pipeline{
+        agent any
+
+        stages{
+            stage('Lint Checks') {
+                steps{
+                    script{
+                       lintChecks()
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}
