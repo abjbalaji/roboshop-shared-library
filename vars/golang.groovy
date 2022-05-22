bc74bf5dc3cf8999fc92183940317f7320e90181
Balaji A <abj.balaji@gmail.com>
2022-05-22 11:13:20 +0530

    Updating

---
+++ vars/golang.groovy
@@ -0,0 +1,43 @@
+def lintChecks()
+{
+    sh '''
+      # ~/node_modules/jslint/bin/jslint.js server.js
+       echo Link checks for ${COMPONENT}
+       '''
+}
+
+
+
+
+def call(){
+    pipeline{
+        agent any
+
+        environment {
+            SONAR = credentials('SONAR')
+        }
+
+        stages{
+            stage('Lint Checks') {
+                steps{
+                    script{
+                        lintChecks()
+                    }
+
+                }
+            }
+
+            stage('Sonar Checks') {
+                steps{
+                    script{
+                        env.ARGS= "-Dsonar.sources=."
+                        common.sonarChecks()
+                    }
+
+                }
+            }
+        }
+    }
+
+
+}
